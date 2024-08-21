66b8d4ed23688d41a68f8069832ecfdbb4e04225
Erlend Markussen Saltnes <erled@msaltnes.no>
2024-08-21 13:18:43 +0200

    Quartz sync: Aug 21, 2024, 1:18 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "no-NB",
+    locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
