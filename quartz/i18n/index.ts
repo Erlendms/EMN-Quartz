2f66ef1bbaf913ce1348819d941f358d9ddfba4b
Erlend Markussen Saltnes <erled@msaltnes.no>
2024-08-21 13:23:08 +0200

    Quartz sync: Aug 21, 2024, 1:23 PM

--- quartz/i18n/index.ts
+++ quartz/i18n/index.ts
@@ -19,6 +19,7 @@ import pt from "./locales/pt-BR"
 import hu from "./locales/hu-HU"
 import fa from "./locales/fa-IR"
 import pl from "./locales/pl-PL"
+import noNB from "./locales/no-NB"
 
 export const TRANSLATIONS = {
   "en-US": enUs,
@@ -62,6 +63,7 @@ export const TRANSLATIONS = {
   "hu-HU": hu,
   "fa-IR": fa,
   "pl-PL": pl,
+  "no-NB": noNB,
 } as const
 
 export const defaultTranslation = "en-US"
